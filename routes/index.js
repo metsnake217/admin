a38af32a682fa2648fd1b317ea52c4a76dd63d83
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-03-02 17:56:48 -0500

    fix query tool

--- routes/index.js
+++ routes/index.js
@@ -899,7 +899,6 @@ totalshares = t[0].counting;
 			var messageStr = "";
 			labYokeSearch.query(function(error, results) {
 				console.log("results " + results.length);
-				console.log("error " + results[1]);
 				var err = "";
 				if(results != null && results.length > 1){
 					console.log("error " + results[1]);
