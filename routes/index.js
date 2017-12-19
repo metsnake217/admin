c35a0fb9d84bddcc91b610ba7b81345df44329b2
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-12-19 02:14:44 -0500

    retrieve users for labs

--- routes/index.js
+++ routes/index.js
@@ -724,16 +724,19 @@ module.exports = function(router) {
 					user0[prop].then(data=>{
 					vennusers.push(data);
 			        //console.log("data is: " + prop + " - " +  JSON.stringify(data));
-			        //if(prop == (users.length - 1)){
-			    	//}
+			        
 				    }).catch(e=>{
 				        //handle error case here when your promise fails
 				        console.log("error from promise: " +e)
+				    }).then(() => {
+				    	if(prop == (users.length - 1)){
+			console.log("vennusers is: " +  JSON.stringify(vennusers));
+			res.render('departments', {vennuser:vennusers, admins:results[5],labadmins: results[4], labs: results[3], vennsettings: results[2], users: results[1], section:"all", depts: results[0], labyoker : req.session.user, loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Departments'});
+
+			    	}
 				    });
 				}
 			}
-			console.log("vennusers is: " +  JSON.stringify(vennusers));
-			res.render('departments', {vennuser:vennusers, admins:results[5],labadmins: results[4], labs: results[3], vennsettings: results[2], users: results[1], section:"all", depts: results[0], labyoker : req.session.user, loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Departments'});
 
 /*
 			Promise.all(users).then(function(dataArr){
