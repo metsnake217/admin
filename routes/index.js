263155c583136577d8553b1453f51f5e7b21fec4
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-12-18 23:35:22 -0500

    retrieve users for labs

--- routes/index.js
+++ routes/index.js
@@ -716,24 +716,26 @@ module.exports = function(router) {
 		labYokeGlobal.finddepartments(function(error, results) {
 			var users = results[2].users;
 			console.log("Venn Settings: " + JSON.stringify(results[2]));
-		for(var prop0 in users){
-			var user0 = users[prop0];
-			console.log("Venn Settings users raw: " + prop0 + " - " + user0);
-			console.log("Venn Settings users length: " + user0.length);
-			for(var prop in user0){
-			user0[prop].then(data=>{
-			vennusers.push(data);
-	        console.log("data is: " + prop + " - " +  JSON.stringify(data));
-	        if(prop == (user0.length - 1)){
-	        	    console.log("vennusers is: " + prop + " - " +  JSON.stringify(vennusers));
-	        	res.render('departments', {vennuser:vennusers, admins:results[5],labadmins: results[4], labs: results[3], vennsettings: results[2], users: results[1], section:"all", depts: results[0], labyoker : req.session.user, loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Departments'});
-	    	}
-		    }).catch(e=>{
-		        //handle error case here when your promise fails
-		        console.log("error from promise: " +e)
-		    });
+			
+			for(var prop0 in users){
+				var user0 = users[prop0];
+				console.log("Venn Settings users raw: " + prop0 + " - " + user0);
+				console.log("Venn Settings users length: " + user0.length);
+				for(var prop in user0){
+					user0[prop].then(data=>{
+					vennusers.push(data);
+			        //console.log("data is: " + prop + " - " +  JSON.stringify(data));
+			        if(prop0 == (users.length - 1)){
+			        	    console.log("vennusers is: " + prop + " - " +  JSON.stringify(vennusers));
+			        	res.render('departments', {vennuser:vennusers, admins:results[5],labadmins: results[4], labs: results[3], vennsettings: results[2], users: results[1], section:"all", depts: results[0], labyoker : req.session.user, loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Departments'});
+			    	}
+				    }).catch(e=>{
+				        //handle error case here when your promise fails
+				        console.log("error from promise: " +e)
+				    });
+				}
 			}
-		}
+			
 			//res.render('departments', {admins:results[5],labadmins: results[4], labs: results[3], vennsettings: results[2], users: results[1], section:"all", depts: results[0], labyoker : req.session.user, loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Departments'});
 			req.session.messages = null;
 		});
