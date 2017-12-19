af9b05e8f9f4b9d2c58de968f8b1b96b647db7dc
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-12-19 11:37:59 -0500

    retrieve users for labs

--- routes/index.js
+++ routes/index.js
@@ -731,8 +731,9 @@ module.exports = function(router) {
 				        //handle error case here when your promise fails
 				        console.log("error from promise: " +e)
 				    }).then(() => {
-
-				    	if(i == (users.length) * (user0.length) ){
+				    	var limit = (users.length) * (user0.length);
+				    	console.log("limit: " + limit);
+				    	if(i == limit ){
 			console.log("vennusers is: " +  JSON.stringify(vennusers));
 			res.render('departments', {vennuser:vennusers, admins:results[5],labadmins: results[4], labs: results[3], vennsettings: results[2], users: results[1], section:"all", depts: results[0], labyoker : req.session.user, loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Departments'});
 
