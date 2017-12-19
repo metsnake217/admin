4b94d65a45bb29d0c60769db5d8704d12f6b98cb
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-12-19 00:43:14 -0500

    retrieve users for labs

--- routes/index.js
+++ routes/index.js
@@ -746,11 +746,11 @@ Promise.all(data).then(function(data0){
 	     		vennusers.push(data0)
 	 });
 	     	    
-    });
-
-	 		}).then((output) => {
+    }).then((output) => {
 	 			console.log("data vennusers is: " +  JSON.stringify(vennusers));
 res.render('departments', {vennuser:vennusers, admins:results[5],labadmins: results[4], labs: results[3], vennsettings: results[2], users: results[1], section:"all", depts: results[0], labyoker : req.session.user, loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Departments'});
+
+	 		});
 
 	 		});
 			//res.render('departments', {admins:results[5],labadmins: results[4], labs: results[3], vennsettings: results[2], users: results[1], section:"all", depts: results[0], labyoker : req.session.user, loggedIn : true, isLoggedInAdmin: req.session.admin, title:'Departments'});
